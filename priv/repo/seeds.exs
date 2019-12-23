97c1f3c0ba8305f2caad13bdf067b350cf7c88c1
José Victor Ferreira <josevictorferreira@outlook.com>
2019-12-23 16:23:34 -0300

    Fixed seeds when the database is already seeded

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,24 +1,22 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     BetManager.Repo.insert!(%BetManager.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
-
 alias BetManager.Seeds.SeedsCountry
 alias BetManager.Seeds.SeedsCurrency
 alias BetManager.Seeds.SeedsSport
 alias BetManager.Seeds.SeedsBookmaker
+alias BetManager.Country
+alias BetManager.Currency
+alias BetManager.Sport
+alias BetManager.Bookmaker
+alias BetManager.Repo
 
-SeedsCountry.seed!()
-
-SeedsCurrency.seed!()
-
-SeedsSport.seed!()
-
-SeedsBookmaker.seed!()
+if Country |> Repo.all() == [] do
+  SeedsCountry.seed!()
+end
+if Currency |> Repo.all() == [] do
+  SeedsCurrency.seed!()
+end
+if Sport |> Repo.all() == [] do
+  SeedsSport.seed!()
+end
+if Bookmaker |> Repo.all() == [] do
+  SeedsBookmaker.seed!()
+end
